6a0ccf889c7a9eccf42c59d3df829a721954b715
TiloGenova <tilo.oschatz@googlemail.com>
2020-12-05 13:13:34 +0100

    30:22 ecom

--- main/models.py
+++ main/models.py
@@ -34,12 +34,15 @@ class Product(models.Model):
     description = models.CharField(max_length=350)
     text = models.TextField(blank=True)
     price = models.DecimalField(decimal_places=2, max_digits=7)
-    discount_price = models.DecimalField(decimal_places=2, max_digits=7, default=0.00, blank=True)
+    discount_price = models.DecimalField(decimal_places=2, max_digits=7, default=0.00)
     image = models.ImageField(upload_to='portfolio/images/')
     discount = models.BooleanField()
     label = models.CharField(max_length=1, choices=LABEL_CHOICE)
     new = models.BooleanField(default=False)
     url = models.URLField(blank=True)
+
+
+
 
     def __str__(self):
         return self.title
