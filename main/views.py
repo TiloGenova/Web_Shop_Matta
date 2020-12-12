6bfab33f08b4043ccbb65c6783eb1e9a5ba4a7d2
TiloGenova <tilo.oschatz@googlemail.com>
2020-12-12 13:59:55 +0100

    4  31:05

--- main/views.py
+++ main/views.py
@@ -57,7 +57,7 @@ def cart(request):
         #query the parent object(order), the child object in all lower case  (orderitem)
         #  _set.all   > all the order items
 
-    else:
+    else:  #USER IS NOT LOGGED IN 
         try:
             cart = json.loads(request.COOKIES['cart']) # json.loads  to parse it and to turn it back into a python dict
         except:
@@ -67,6 +67,28 @@ def cart(request):
         order = {'get_cart_total':0, 'get_cart_items':0, 'shipping': False}
         cartItems = order['get_cart_items']
 
+
+        for i in cart:   # sum of items to show in cart
+            cartItems +=cart[i]["quantity"]
+
+            product = Product.objects.get(id=i)
+            total = (product.price * cart[i]['quantity'])
+
+            order['get_cart_total'] += total
+            order['get_cart_items'] += cart[i]['quantity']
+            
+            
+            item = {
+                'product':{
+                    'id': product.id,
+                    'name': product.title,
+                    'price': product.price,
+                    'imageURL': product.imageURL,
+                },
+                'quantity': cart[i]["quantity"],
+                'get_total': total
+            }
+            items.append(item)
 
     context = {'items': items, 'order': order,'cartItems':cartItems}
     return render(request, 'main/cart.html', context)
