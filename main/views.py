e37b2c34e2db93115940fc30f3e2c85267d87f81
TiloGenova <tilo.oschatz@googlemail.com>
2020-12-12 19:07:28 +0100

    4 1:06:25

--- main/views.py
+++ main/views.py
@@ -3,7 +3,7 @@ from .models import *
 from django.http import JsonResponse
 import json
 import datetime
-from .utils import cookieCart, cartData
+from .utils import cookieCart, cartData, guestOrder
 
 # Create your views here.
 # https://mdbootstrap.com/freebies/jquery/e-commerce/
@@ -102,27 +102,35 @@ def processOrder(request):
     if request.user.is_authenticated:
         customer = request.user.customer
         order, created = Order.objects.get_or_create(customer=customer, complete=False)
-        total = float(data['form']['total'])
-        order.transaction_id = transaction_id
 
 
-        if total == order.get_cart_total:
-            order.complete = True
-        order.save()
-
-
-        if order.shipping == True:
-            ShippingAddress.objects.create(
-                customer=customer,
-                order=order,
-                address=data['shipping']['address'],
-                city=data['shipping']['city'],
-                state=data['shipping']['state'],
-                zipcode=data['shipping']['zipcode'],
-                country=data['shipping']['country'],
-            )
 
     else:
-        print('User is not logged in...')
+        customer, order = guestOrder(request, data)
+
+
+    # Following block for logged in and not logged in users
+    total = float(data['form']['total'])
+    order.transaction_id = transaction_id
+
+
+    if total == order.get_cart_total:
+        order.complete = True
+    order.save()
+
+
+    if order.shipping == True:
+        ShippingAddress.objects.create(
+            customer=customer,
+            order=order,
+            address=data['shipping']['address'],
+            city=data['shipping']['city'],
+            state=data['shipping']['state'],
+            zipcode=data['shipping']['zipcode'],
+            country=data['shipping']['country'],
+        )
+
+
+
 
     return JsonResponse('Payment complete!', safe=False)
