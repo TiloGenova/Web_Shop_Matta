f8e9fc657aa114c948d41eb5e99e120e64bddd24
TiloGenova <tilo.oschatz@googlemail.com>
2020-12-20 12:21:38 +0100

    5  after registration  Login works  but email/name missing in customer model

--- main/models.py
+++ main/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.conf import settings
 from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 
 class Product(models.Model):
@@ -50,10 +52,27 @@ class Customer(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, null=True, blank=True)
     name = models.CharField(max_length=200, null=True)
     email = models.CharField(max_length=200, null=True)
+    #email = models.CharField(User.email, max_length=200, null=True)
 
     def __str__(self):
         return self.name
-3
+
+
+@receiver(post_save, sender=settings.AUTH_USER_MODEL)
+def create_or_save_customer(sender, created, instance, **kwargs):
+    if created:
+        Customer.objects.create(user=instance)
+    instance.customer.save()
+
+'''
+@receiver(post_save, sender=settings.AUTH_USER_MODEL)
+def create_or_save_customer(sender, created, instance, **kwargs):
+    if created:
+        Customer.objects.create(user=instance)
+    instance.customer.save()'''
+
+
+
 
 
 
