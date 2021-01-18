d55f3075b6049a2cc913a57459988b7b60a66f00
TiloGenova <tilo.oschatz@googlemail.com>
2021-01-18 10:37:30 +0100

    registration done / optische verbesserungen

--- main/forms.py
+++ main/forms.py
@@ -5,8 +5,11 @@ from django.contrib.auth.forms import UserCreationForm
 
 
 class CreateUserForm(UserCreationForm):  #takes basically the django class
+    password2 = forms.CharField(label='Conferma Password',
+    widget = forms.PasswordInput)
     class Meta:
         model = User
         fields = ['username', 'first_name', 'last_name',
                   'email', 'password1', 'password2']
+        labels = {'email': 'Email', 'first_name': 'Nome', 'last_name': 'Cognome', 'password2': 'Conferma Password'}
 #
