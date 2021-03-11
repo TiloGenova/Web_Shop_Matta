54e8c6e8034b2db73341af175c6d8d23b18509fc
TiloGenova <tilo.oschatz@googlemail.com>
2021-03-11 11:41:29 +0100

    ZEROSTOCK2 (Kontrolle, ob Bestnad bei 0) in Sessions                         viewes.py  line 340
                            Session storage works not completely live   sessions saved too long ? not deleting?
    
                            SEO
                            Alt added  home/ base / about

--- mysite/urls.py
+++ mysite/urls.py
@@ -2,16 +2,12 @@ from django.contrib import admin
 from django.urls import path, include
 
 
-
 app_name = 'main'
-
-
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('main.urls', namespace='main')),
-
 
 
 ]
